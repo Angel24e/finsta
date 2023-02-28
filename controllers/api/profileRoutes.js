232abcd5c23a67c58a9035226961ca70fe4513ea
marksalazar <105082689+marksalazr@users.noreply.github.com>
2023-02-27 20:29:21 -0800

    routes

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -3,8 +3,16 @@ const { Profile } = require('../../models');
 
 router.post('/', withAuth, async (req, res) => {
     try {
+        const userProfile = await Profile.create(req.body);
+        req.session.save(() => {
+            req.session.profile_id= userProfile.id ;
+            req.session.logged_in=true; 
+            
+            res.status(200).json(userProfile);
+        } );
 
     } catch (err) {
+        res.status(400).json(err);
 
     }
 });
