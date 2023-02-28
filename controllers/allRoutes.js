9f4d565e3b5aa51a670b2c3f2bdc09d319c37fbc
angel Escamilla <angel4esc@gmail.com>
2023-02-27 19:40:27 -0700

    Boiler plate controller/route folder

---
+++ controllers/allRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Posts, Profile } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
+
+module.exports = router;
