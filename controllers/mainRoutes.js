bb42f2300f23aab07f432423b150b2fab922145b
angel Escamilla <angel4esc@gmail.com>
2023-03-02 18:22:20 -0700

    Few syntax errors

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -3,71 +3,71 @@ const { Post, Profile } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
-    // Gets all posts and joins with profile data by name.
-    try { 
-        const postData = await Post.findAll({
-            include: [ {
-                model: Profile, 
-                attributes: ['name'],
-            },
-        ],
-        });
-        // Serializationnn 
-        const posts = postData.map((post)=> post.get ({plain: true}));
+  // Gets all posts and joins with profile data by name.
+  try {
+    const postData = await Post.findAll({
+      include: [{
+        model: Profile,
+        attributes: ['name'],
+      },
+      ],
+    });
+    // Serializationnn 
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-        // passing serialized data and session flag into template
-        res.render('main',{
-            posts,
-            logged_in: req.session.logged_in
-        });
+    // passing serialized data and session flag into template
+    res.render('main', {
+      posts,
+      logged_in: req.session.logged_in
+    });
 
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/post/:id', withAuth, async (req, res) => { 
-    try {  
-      const postData = await Post.findByPk(req.params.id, {
-        include: [
-          {
-            model: Post,
-            attributes: ['name'],
-          },
-        ],
-      });
-      const post = postData.get({ plain: true });
-  
-      res.render('posts', {
-        ...post,
-        logged_in: req.session.logged_in
-      });
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Post,
+          attributes: ['name'],
+        },
+      ],
+    });
+    const post = postData.get({ plain: true });
 
-    } catch (err) {
-      res.status(500).json(err)
-    }
+    res.render('posts', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 
-router.get('/profile', withAuth, async (req,res) => {
+router.get('/profile', withAuth, async (req, res) => {
   try {
     //finds profile based on the session ID
     const profileData = await Profile.findByPk(req.session.user_id, {
-      attributes: {exclude: ['password']},
-      include: [{model: Post }],
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
     });
-    const profile = profileData.get({plain:true});
-    res.render('profile',{
+    const profile = profileData.get({ plain: true });
+    res.render('profile', {
       ...profile,
       logged_in: true
     });
-  } catch (err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 // if user is already logged in, redirects the request to another dimension
-router.get('/login',(req,res)=>{
-  if(req.session.logged_in){
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
     res.redirect('/profile');
     return;
   }
